c8657c20647dea715dc2a8ff2df2032fc054b366
Frederick Samson <samsonfr@gmail.com>
2016-11-21 09:52:21 -0500

    Initial Version

---
+++ NLogNetworkTargetTest/FrmTest.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace NLogNetworkTargetTest
+{
+  partial class FrmTest
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.cmdLog = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // cmdLog
+      // 
+      this.cmdLog.Location = new System.Drawing.Point(12, 12);
+      this.cmdLog.Name = "cmdLog";
+      this.cmdLog.Size = new System.Drawing.Size(75, 23);
+      this.cmdLog.TabIndex = 0;
+      this.cmdLog.Text = "Log";
+      this.cmdLog.UseVisualStyleBackColor = true;
+      // 
+      // FrmTest
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(411, 44);
+      this.Controls.Add(this.cmdLog);
+      this.Name = "FrmTest";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "NLog Network Target Test";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button cmdLog;
+  }
+}
+
---
+++ NLogNetworkTargetTest/FrmTest.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+using NLog;
+
+namespace NLogNetworkTargetTest
+{
+    public partial class FrmTest : Form
+    {
+        public FrmTest()
+        {
+            InitializeComponent();
+            cmdLog.Click += CmdLog_Click;
+        }
+
+        private void CmdLog_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Logger pLogger = LogManager.GetCurrentClassLogger();
+                pLogger.Trace(new String('-', 120));
+                pLogger.Fatal("Fatal");
+                pLogger.Error(new ApplicationException("Exception Text"), "Error");
+                pLogger.Warn("Warning");
+                pLogger.Info("Info");
+                pLogger.Debug("Debug");
+                pLogger.Trace("Trace");
+            }
+            catch (Exception pException)
+            {
+                MessageBox.Show(pException.Message);
+            }
+        }
+    }
+}
